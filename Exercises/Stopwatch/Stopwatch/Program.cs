a09c6b43ab2ae040b1788932d4c054dd9957b6f7
Cody Sheridan <sheridan.codyj@gmail.com>
2021-04-12 15:59:29 -0400

    Complete Section 1-Exercise 1

---
+++ Exercises/Stopwatch/Stopwatch/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+
+            var stopwatch = new Stopwatch();
+
+            for (var i = 0; i < 2; i++)
+            {
+                stopwatch.Start();
+
+                Thread.Sleep(1000);
+
+                stopwatch.Stop();
+
+                Console.WriteLine("Duration " + stopwatch.Duration());
+
+                Console.WriteLine("Press Enter to run the stopwatch one more time.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Exercises/Stopwatch/Stopwatch/Stopwatch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Stopwatch
+{
+    public class Stopwatch
+    {
+        private DateTime _startTime;
+        private DateTime _stopTime;
+        private bool _running;
+
+        public void Start()
+        { 
+            if (_running)
+                throw new InvalidOperationException("Stopwatch is arleady running");
+            
+
+            _startTime = DateTime.Now;
+            _running = true;
+        }
+        
+        public void Stop()
+        {
+            if (!_running)
+                throw new InvalidOperationException("Stopwatch is not running.");
+            
+            _stopTime = DateTime.Now;
+            _running = false;
+        }
+
+        public TimeSpan Duration()
+        {
+            return _stopTime - _startTime;
+        }
+    }
+}
+
