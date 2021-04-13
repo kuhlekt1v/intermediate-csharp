da2eb8e7bd7d2d26fb49e40669adc248d92b7abb
Cody Sheridan <sheridan.codyj@gmail.com>
2021-04-13 12:58:15 -0400

    Sec.1 Ex. 2 - My Solution.

---
+++ Exercises/StackOverflow/StackOverflow/Post.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StackOverflow
+{
+    public class Post
+    {
+        public int VoteCount { get; private set; }
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public DateTime DateTimeCreated { get; } = DateTime.Now;
+
+
+        public Post(string title, string desc)
+        {
+            Title = title;
+            Description = desc;
+        }
+
+
+        public void UpVote()
+        {
+            if (VoteCount >= 0)
+                VoteCount += 1;
+        }
+
+        public void DownVote()
+        {
+            if (VoteCount >= 1)
+                VoteCount -= 1;
+        }
+
+    }
+}
---
+++ Exercises/StackOverflow/StackOverflow/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace StackOverflow
+{
+
+class Program
+    {
+        static void Main(string [] args)
+        {
+            // Create new post.
+            var post = new Post("First Post", "First Description");
+
+            // Cast votes.
+            post.UpVote();
+            post.UpVote();
+            post.UpVote();
+            post.UpVote();
+            post.UpVote();
+            post.UpVote();
+            post.DownVote();
+            post.DownVote();
+            post.DownVote();
+            post.DownVote();
+            post.DownVote();
+
+            // Display post.
+            Console.WriteLine($"{post.Title} - {post.Description} (Posted: {post.DateTimeCreated})\nTotal Votes: {post.VoteCount}");
+        }
+    }
+}
